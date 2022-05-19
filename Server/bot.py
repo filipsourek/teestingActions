a538bf16864060607c0e909d70798c94f2341536
filipsourek <40028243+filipsourek@users.noreply.github.com>
2022-05-19 16:42:11 +0200

    Update bot.py

--- Server/bot.py
+++ Server/bot.py
@@ -10,7 +10,7 @@ HELP_MSG = "List of requests: \n time - returns current time \n name - returns n
 
 class Bot:
     def __init__(self):
-        self.name = "Eduard"
+        self.name = "Pepicek"
         self.values = dict()
         self.values["28.04.2022"] = str(5)
         
